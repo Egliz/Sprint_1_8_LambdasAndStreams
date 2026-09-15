4e09c5eebfd4d298cb25b628106671e1ca538e92
Egliz <egliz3010@gmail.com>
2026-09-15 18:58:16 +0200

    feat: add method that prints months using method reference

--- src/main/java/Level_1/ex4_printWithReference/PrintWithReference.java
+++ src/main/java/Level_1/ex4_printWithReference/PrintWithReference.java
@@ -1,15 +1,15 @@
-package Level_1.ex3_printMonths;
+package Level_1.ex4_printWithReference;
 
 import java.util.List;
 
-public class PrintMonths {
+public class PrintWithReference {
     public static void main(String[] args) {
         List<String> months = List.of("January", "February", "March", "April", "May",
                 "June", "July", "August", "September", "October", "November", "December");
-        printMonths(months);
+        printWithMethodReference(months);
     }
 
-    public static void printMonths(List<String> months){
-       months.forEach(month -> System.out.println(month));
+    public static void printWithMethodReference(List<String> months){
+        months.forEach(System.out::println);
     }
 }

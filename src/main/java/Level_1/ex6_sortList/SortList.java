0580967bab47dd6993ae1ba246dbc7610b4b7b0d
Egliz <egliz3010@gmail.com>
2026-09-15 19:49:46 +0200

    feat: add method that sorts a list from lowest to highest

---
+++ src/main/java/Level_1/ex6_sortList/SortList.java
@@ -0,0 +1,18 @@
+package Level_1.ex6_sortList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("5", "book", "12", "100",
+                "notebook",
+                "computer"));
+
+        sortList(list);
+        System.out.println(list);
+    }
+    public static void sortList(List<String> list){
+        list.sort((a, b) -> a.length() - b.length());
+    }
+}

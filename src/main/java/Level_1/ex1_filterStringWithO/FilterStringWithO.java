dac65906b00322dbef2b296ca6abeaa035f82047
Egliz <egliz3010@gmail.com>
2026-09-15 17:51:46 +0200

    feat: add method to filter the list of strings by length and containing 'o'

--- src/main/java/Level_1/ex1_filterStringWithO/FilterStringWithO.java
+++ src/main/java/Level_1/ex1_filterStringWithO/FilterStringWithO.java
@@ -1,12 +1,10 @@
-package Level_1.ex1_ex2;
+package Level_1.ex1_filterStringWithO;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
 
-
-public class ex1_ex2 {
+public class FilterStringWithO {
 
     public static void main(String[] args) {
         List<String> names = List.of("Ana", "Hugo", "Carmen", "Leo", "Lucas");
@@ -15,6 +13,8 @@ public class ex1_ex2 {
 
     }
     public static List<String> stringContainsO(List<String> names) {
-        return names.stream().filter(s -> s.contains("o")).collect(Collectors.toList());
+        return names.stream()
+                .filter(s -> s.contains("o"))
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/Level_1/ex2_filterStringLength/FilterStringLength.java
@@ -0,0 +1,19 @@
+package Level_1.ex2_filterStringLength;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterStringLength {
+    public static void main(String[] args) {
+        List<String> names = List.of("Ana", "Hugo", "Carmen", "Leo", "Lucas", "Antonio");
+
+        System.out.println(filterStrByLength(names));
+
+    }
+    public static List<String> filterStrByLength(List<String> names) {
+        return names.stream()
+                .filter(s -> s.contains("o") && s.length() > 5)
+                .collect(Collectors.toList()
+                );
+    }
+}

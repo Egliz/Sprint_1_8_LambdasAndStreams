d6ddadca7b64f10f59892c3f4e0a21a8b61d767f
Egliz <egliz3010@gmail.com>
2026-09-15 19:52:57 +0200

    feat: add method that sorts a list from highest to lowest

---
+++ src/main/java/Level_1/ex7_reverseList/ReverseList.java
@@ -0,0 +1,18 @@
+package Level_1.ex7_reverseList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("5", "book", "12", "100",
+                "notebook",
+                "computer"));
+
+        sortListInReverse(list);
+        System.out.println(list);
+    }
+    public static void sortListInReverse(List<String> list){
+        list.sort((a, b) -> b.length() - a.length());
+    }
+}
